7563e9c642c464cfb23c56102978958c1b62cc60
Laura Felce <crepu83@yahoo.it>
2019-07-09 09:49:04 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def home
-    redirect_to controller: 'sessions', action: 'new' unless session[:name]
+    redirect_to controller: 'sessions', action: 'new' unless current_user
   end
 
   def new
