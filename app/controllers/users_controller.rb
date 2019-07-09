31082db56836290c619c11d3709ac8a8434efbae
Laura Felce <crepu83@yahoo.it>
2019-07-09 08:29:19 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
     redirect_to controller: 'sessions', action: 'new' unless session[:name]
   end
 
+  def new
+  end  
+  
   def create
     User.create(user_params)
   end
